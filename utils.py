72abb399a68dc7b701d4b2279fe8191e27cb51a9
yocef-Z <josef.zeininger@gmail.com>
2026-03-16 10:52:24 +0100

    added normalize_column function

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 """
 Celsius - Fahrenheit converter
 """
@@ -10,8 +13,15 @@ def celsius_to_fahrenheit(celsius):
 """
 Filling Data Frame NaN - set missing Data Points in column to mean value
 """
-import pandas as pd
 
 def fill_missing_mean(df, column):
     df[column] = df[column].fillna(df[column].mean())
     return df
+
+"""
+Normalize Data Frame between -1 and 1 - USEFULL IF VALUES ARE >0
+"""
+
+def normalize_column(df, column):
+    #rescaling by dividing by the maximum absolute value of the column
+    df[column] = df[column] / df[column].abs().max()
