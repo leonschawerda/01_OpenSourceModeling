caead05af5a0003a3b2db1d30b0bb0726144f0d9
yocef-Z <josef.zeininger@gmail.com>
2026-03-16 09:57:44 +0100

    added fill_missing_mean function

--- utils.py
+++ utils.py
@@ -6,3 +6,12 @@ def celsius_to_fahrenheit(celsius):
     """Converts Celsius into Fahrenheit"""
     return (celsius * 9/5) + 32
 
+
+"""
+Filling Data Frame NaN - set missing Data Points in column to mean value
+"""
+import pandas as pd
+
+def fill_missing_mean(df, column):
+    df[column] = df[column].fillna(df[column].mean())
+    return df
