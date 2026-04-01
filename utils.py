b8a7c98ee55efed3492a4bee307409bdb535686d
Leon Schawerda <e11709119@student.tuwien.ac.at>
2026-04-01 21:51:26 +0200

    added spaces

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ Celsius - Fahrenheit converter
 
 def celsius_to_fahrenheit(celsius):
     #Converts Celsius into Fahrenheit
-    return (celsius*9/5) + 32
+    return (celsius * 9/5) + 32
 
 """
 Filling Data Frame NaN - set missing Data Points in column to mean value
